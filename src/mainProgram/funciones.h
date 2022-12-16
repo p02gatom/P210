c9f4bad6e0d3326bf9bd0290c1b83100a8e8af43
i12molml <i12molml@uco.es>
2022-12-16 22:55:52 +0100

    Modificaciones

--- src/mainProgram/funciones.h
+++ src/mainProgram/funciones.h
@@ -6,6 +6,7 @@
 #include "registered_user.h"
 
 void load_users_database(std::list<User> &users);
+void save_users_database(std::list<User> users);
 User log_in(std::list<User> users);
 User sign_up(std::list<User> &users);
 
