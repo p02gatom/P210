456a6aeab18926a0aa91375054a42095fcb2c84e
p02gatom <91120174+p02gatom@users.noreply.github.com>
2022-12-16 17:53:31 +0100

    Update student.h

--- src/student/student.h
+++ src/student/student.h
@@ -5,16 +5,31 @@
 #define STUDENT_H
 
 #include "registered_user.h"
+#include "course.h"
 #include <string>
+#include <list>
 
-class Student : public Registered_user
-{
+class Student : public Registered_user{
+
 	private:
 
 	public:
 
-		void singup_course();
+		Student(
 
-}
+			std::string name="empty",
+			std::string username="empty",
+			std::string email="empty",
+			std::string password="empty",				
+			std::string user_type="empty"):Registered_user(name, username, email, password, user_type);
+
+	bool singup_course(std::string c1, std::list <Ext_Course> course_list);
+	void modify_student(Student s);
+	bool delete_student(Student s);
+	std::list <Ext_Course> get_course_list();
+	std::list <Ext_Course> get_mycourses();
+	
+
+};
 
 #endif // STUDENT_H
